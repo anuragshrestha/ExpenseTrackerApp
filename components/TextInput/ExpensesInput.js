5f7d6f9fb6ee89a0513523b7b03464549187bd76
Anurag Shrestha <ashrestha@unm.edu>
2024-02-11 22:20:53 -0700

    Updated ExpensesInput file

--- components/TextInput/ExpensesInput.js
+++ components/TextInput/ExpensesInput.js
@@ -1,8 +1,26 @@
 import { Keyboard, View ,TextInput} from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpensesInput() {
-  function amountchangeHandler() {}
+
+ const[inputValue, setInputValue] = useState({
+  amount : '',
+  date: '',
+  description: ''
+ })
+
+  function amountchangeHandler(identifier, enteredValue) {
+    
+    setInputValue((value) => {
+      return{
+        ...value, 
+        [identifier] : enteredValue
+      };
+    });
+
+
+  }
   return (
     <View>
       <Input
@@ -10,19 +28,24 @@ function ExpensesInput() {
         inputConfig={{
           autoCorrect :  false,
           multiline: true,
+          onChangeText: amountchangeHandler.bind(this,'description'),
+          value : inputValue.description,
         }}
       />
       <Input
         children="Amount"
         inputConfig={{
          maxLength : 10,
-          onChangeText: amountchangeHandler,
-          keyboardType: 'number-pad'
+          onChangeText: amountchangeHandler.bind(this,'amount'),
+          value : inputValue.amount,
+          keyboardType: 'number-pad',
+        
         }}
       />
       <Input children="Date" inputConfig={{
         KeyboardType: 'default',
-        onChangeText: () => {}
+        onChangeText: amountchangeHandler.bind(this,'date'),
+        value : inputValue.date,
       }}/>
     </View>
   );
