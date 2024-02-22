e39e391e8f04953306de1568dc3bce579d9375b1
Anurag Shrestha <ashrestha@unm.edu>
2024-02-21 20:59:30 -0700

    updated http file

--- util/http.js
+++ util/http.js
@@ -9,4 +9,20 @@ export function storeExpenses(expenseData){
 export async function fetchExpenses(){
   const response = await  axios.get("https://reactnative-expense-app-default-rtdb.firebaseio.com/expenses.json");
 
+
+    const expenses = [ ];
+
+
+    for(const key in response.data){
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            description : response.data[key].description,
+            date : new Date(response.data[key].date),
+        };
+
+        expenses.push(expenseObj);
+    }
+
+     return expenses;
 }
