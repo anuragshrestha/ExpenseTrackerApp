08bcf806ebd041cbd09723a7be970934bc7215d0
Anurag Shrestha <ashrestha@unm.edu>
2024-02-21 20:58:46 -0700

    Updated recent expenses file

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,28 +1,31 @@
-import {Text }from 'react-native'
-import ExpensesOutput from '../components/ExpensesOutput.js/ExpensesOutput'
-import { useContext, useEffect } from 'react';
-import { ExpensesContext } from '../store/expenses-contextAPI';
-import { getDate } from '../util/date';
- import { fetchExpenses } from '../util/http';
- 
+import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput.js/ExpensesOutput";
+import { useContext, useEffect, useState } from "react";
+import { ExpensesContext } from "../store/expenses-contextAPI";
+import { getDate } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
- 
-function RecentExpenses(){
+function RecentExpenses() {
 
-useEffect(( ) =>{
-   fetchExpenses();
-}, []);
 
    const expenseContext = useContext(ExpensesContext);
-   const recentExpenses = expenseContext.expenses.filter((expense) => {
-     const today =  new Date();
-     const sevenDays = getDate(today, 7);
 
-     return (expense.date >= sevenDays) && (expense.date <= today);
-   })
-   return(
-      <ExpensesOutput expenses={recentExpenses} periodName= "Last 7 days "/>
-   )
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+           expenseContext.setExpenses(expenses);
+    }
+    getExpenses();
+  }, []);
+
+  
+  const recentExpenses = expenseContext.expenses.filter((expense) => {
+    const today = new Date();
+    const sevenDays = getDate(today, 7);
+
+    return expense.date >= sevenDays && expense.date <= today;
+  });
+  return <ExpensesOutput expenses={recentExpenses} periodName="Last 7 days " />;
 }
 
- export default RecentExpenses;
+export default RecentExpenses;
