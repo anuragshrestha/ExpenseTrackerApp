1844b8d8f97e1a46dbf68ab46927101e27b0e21c
Anurag Shrestha <ashrestha@unm.edu>
2024-02-22 21:42:45 -0700

    added the activity indicator functions

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -4,21 +4,27 @@ import { useContext, useEffect, useState } from "react";
 import { ExpensesContext } from "../store/expenses-contextAPI";
 import { getDate } from "../util/date";
 import { fetchExpenses } from "../util/http";
+import OverLoading from "../util/OverLoading";
 
 function RecentExpenses() {
 
-
+   const [isfetching, setFetching] =  useState([true]);
    const expenseContext = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
+      setFetching(true);
       const expenses = await fetchExpenses();
+      setFetching(false);
            expenseContext.setExpenses(expenses);
     }
     getExpenses();
   }, []);
 
   
+   if(isfetching){
+    return <OverLoading/>
+   }
   const recentExpenses = expenseContext.expenses.filter((expense) => {
     const today = new Date();
     const sevenDays = getDate(today, 7);
